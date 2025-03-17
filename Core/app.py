260d907740249132c8ebe028d4414598782c8cbd
Leonardo Costa <l.j.m.costa@hotmail.com>
2025-03-17 20:31:54 +0000

    chunck added

--- Core/app.py
+++ Core/app.py
@@ -1,5 +1,9 @@
+import re
 import gradio
 from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_text_splitters import CharacterTextSplitter
 
 
 def predict(message, history):
@@ -9,7 +13,26 @@ def predict(message, history):
     loader = PyPDFLoader(file_path, mode="single")
     docs = loader.load()
 
+    # Step 1.5 - Document cleaning
+    for doc in docs:
+        # \n ou break lines ou enters, para novos paragrafos é valido e devemos deixar, porque é nova informação
+        # o resto poderá ser apagado
+        doc.page_content = re.sub('\n', '', doc.page_content)
+
     print(docs)
+
+    # Step 2 - Document Transfrom / Splitting
+    chunksize = 500
+    chunkoverlap = chunksize * 0.2
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Este só se usa no CharacterTextSplitter
+        # separator= '. \n',
+        chunk_size=chunksize,
+        chunk_overlap=chunkoverlap
+    )
+
+    chunks = text_splitter.split_documents(docs)
+    print(chunks)
 
     return "olá!"
 
